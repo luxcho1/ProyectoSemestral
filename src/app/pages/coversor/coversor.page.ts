843a2d6977be010f0b994b661192e87dc24a944e
luis <fernando2001th@gmail.com>
2022-11-29 21:21:20 -0300

    Implementacion conversor

--- src/app/pages/coversor/coversor.page.ts
+++ src/app/pages/coversor/coversor.page.ts
@@ -11,7 +11,16 @@ export class CoversorPage implements OnInit {
 
   pageTitle = 'Conversor';
   isNotHome = true;
-  listadoDatos:any = [];
+  resultados: any;
+  
+
+  dolar: any;
+  euro: any;
+  uf: any;
+  resultado: number;
+  valor: number;
+  tipoMoneda: string;
+  resultadoTotal: any;
 
   
 
@@ -24,12 +33,63 @@ export class CoversorPage implements OnInit {
   cargarDatosConvertidor(){
     this.mindicadorclService.obtenerListadoConvertidor()
     .then(respuesta => {
-      this.listadoDatos = [respuesta.uf,respuesta.dolar,respuesta.euro];
-      console.log(this.listadoDatos);
+      this.resultados = respuesta;
+      this.dolar = this.resultados.dolar.valor;
+      this.euro = this.resultados.euro.valor;
+      this.uf = this.resultados.uf.valor;
+      console.log(this.resultados); 
     },
     (err) => {
       console.log(err);
     });
   }
-  
+
+  obtenerValorMoneda(tipoMoneda){
+    if(tipoMoneda == "dolares"){
+      this.resultado = this.valor / this.dolar
+      this.resultadoTotal = this.resultado.toFixed(2)
+    }
+    else if(tipoMoneda == "euros"){
+      this.resultado = Math.round(this.valor / this.euro)
+      this.resultadoTotal = this.resultado.toFixed(2)
+    }
+    else if(tipoMoneda == "ufs"){
+      this.resultado = Math.round(this.valor / this.uf)
+      this.resultadoTotal = this.resultado.toFixed(2)
+    }
+  }
 }
+
+
+
+// cargarValorMonedas(){
+//   this.mindicadorService.obtenerValorPesos()
+//   .then(results => {
+//     this.results = results;
+//     this.valorMonedaDolar = this.results.dolar.valor;
+//     this.valorMonedaEuro = this.results.euro.valor;
+//     this.valorMonedaUF = this.results.uf.valor;
+//     console.log(this.results); 
+//   },
+//   (err) => {
+//     console.log(err);
+//   });
+// }
+
+// obtenerValorMoneda(moneda){
+//   if(moneda == "dolares"){
+//     this.resultado = this.valor / this.valorMonedaDolar
+//     this.resultado1 = this.resultado.toFixed(2)
+//   }
+//   else if(moneda == "euros"){
+//     this.resultado = Math.round(this.valor / this.valorMonedaEuro)
+//     this.resultado1 = this.resultado.toFixed(2)
+//   }
+//   else if(moneda == "ufs"){
+//     this.resultado = Math.round(this.valor / this.valorMonedaUF)
+//     this.resultado1 = this.resultado.toFixed(2)
+//   }
+//   else {
+//     this.presentAlert();
+//   }
+// }
